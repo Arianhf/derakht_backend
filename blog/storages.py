884b95a242880e027b0798d775d0d6e3f5ce7cfc
Arian Hedayati Far <arian.hedayatifar@gmail.com>
2024-11-05 14:04:31 +0330

    update storage settings

--- blog/storages.py
+++ blog/storages.py
@@ -6,7 +6,7 @@ from django.conf import settings
 class StaticStorage(S3Boto3Storage):
     bucket_name = settings.AWS_STATIC_BUCKET_NAME
     default_acl = 'public-read'
-    custom_domain = settings.AWS_S3_CUSTOM_DOMAIN + '/static/'
+    custom_domain = settings.AWS_S3_CUSTOM_DOMAIN + '/static'
     endpoint_url = settings.AWS_S3_ENDPOINT_URL
     use_ssl = settings.AWS_S3_USE_SSL
     verify = settings.AWS_S3_VERIFY
@@ -15,7 +15,7 @@ class StaticStorage(S3Boto3Storage):
 class MediaStorage(S3Boto3Storage):
     bucket_name = settings.AWS_MEDIA_BUCKET_NAME
     default_acl = 'public-read'
-    custom_domain = settings.AWS_S3_CUSTOM_DOMAIN + '/media/'
+    custom_domain = settings.AWS_S3_CUSTOM_DOMAIN + '/media'
     endpoint_url = settings.AWS_S3_ENDPOINT_URL
     use_ssl = settings.AWS_S3_USE_SSL
     verify = settings.AWS_S3_VERIFY
