b322fe2e2799d676957b87e111278381c5bf885d
Arian Hedayati Far <arian.hedayatifar@gmail.com>
2025-10-28 19:04:03 +0330

    added feature flag admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import FeatureFlag
+
+
+@admin.register(FeatureFlag)
+class FeatureFlagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'enabled', 'created_at', 'updated_at')
+    list_filter = ('enabled', 'created_at')
+    search_fields = ('name', 'description')
+    list_editable = ('enabled',)
+    readonly_fields = ('created_at', 'updated_at')
+
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'enabled', 'description')
+        }),
+        ('Timestamps', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
